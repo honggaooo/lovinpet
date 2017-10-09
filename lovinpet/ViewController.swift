85895da91bd7dd8e556599716bc8cce74ec04c18
honggaooo <hong.gao@marquette.edu>
2017-10-08 20:20:54 -0500

    Initial Commit

---
+++ lovinpet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lovinpet
+//
+//  Created by Hong Gao on 10/8/17.
+//  Copyright © 2017 Hong Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
